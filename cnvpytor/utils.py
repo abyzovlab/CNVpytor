040a8f2b7ccde1aef55e4bfb866e148bf15eca1b
suvakov <suvakov@gmail.com>
2024-07-24 22:07:52 -0500

    Adding -gc_corr_rm_ol option

--- cnvpytor/utils.py
+++ cnvpytor/utils.py
@@ -677,8 +677,8 @@ def calculate_gc_correction(his_rd_gc, mean, sigma, bin_size=1, gc_corr_rm_ol=Fa
     if gc_corr_rm_ol:
         min_bin = int(0.75 * mean / bin_size)
         max_bin = min(int(1.25 * mean / bin_size), his_rd_gc.shape[0])
-    his = his_rd_gc[1:max_bin, :]
-    rd = np.repeat((np.arange(1, max_bin * bin_size, bin_size)[:max_bin - min_bin]).reshape((max_bin - min_bin, 1)), 101, axis=1)
+    his = his_rd_gc[min_bin:max_bin, :]
+    rd = np.repeat((np.arange(min_bin * bin_size, max_bin * bin_size, bin_size)).reshape((max_bin - min_bin, 1)), 101, axis=1)
     np.seterr(divide='ignore', invalid='ignore')
     gc_corr = np.sum(rd * his, axis=0) / np.sum(his, axis=0)
     no_stat = np.isnan(gc_corr)
